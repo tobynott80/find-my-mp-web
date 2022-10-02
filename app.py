615816d2946c738e1c852ff1225bff9eb1aa95da
tobiah nott <notttj@cardiff.ac.uk>
2022-10-02 17:51:50 +0100

    added search

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
-from operator import index
-from flask import Flask, render_template
+
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
     return render_template('index.html')
+
+@app.route('/search')
+def search():
+    postcode = request.args.get('search')
+    return render_template('search.html',
+                           postcode=postcode)
 
 if __name__ == '__main__':
     app.run(debug=True)
