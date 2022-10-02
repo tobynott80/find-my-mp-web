f8486de633cdf40c81b1bfc48e9871d5782c0c7f
tobiah nott <notttj@cardiff.ac.uk>
2022-10-02 17:17:35 +0100

    base html

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return render_template(index)
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
