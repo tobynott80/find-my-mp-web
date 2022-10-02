6e07594f67b18a4c98e2d86ca875b0549e4ede58
tobiah nott <notttj@cardiff.ac.uk>
2022-10-02 17:11:43 +0100

    init file structure

---
+++ app.py
@@ -0,0 +1,10 @@
+from operator import index
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return render_template(index)
+
+if __name__ == '__main__':
+    app.run(debug=True)
