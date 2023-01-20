eab81b385edb9d52dc093a306973758925b7f2cc
toby nott <toby@tobynott.com>
2023-01-20 14:54:45 +0000

    added not found redierect

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 from flask import Flask, render_template, request
 import requests
-import jso
-import jso
+import json
 
 app = Flask(__name__)
 
@@ -14,17 +13,17 @@ def search():
     postcode = request.args.get('search')
     url = "https://members-api.parliament.uk/api/Location/Constituency/Search?searchText="+ postcode
     resp = requests.get(url)
-    data = resp.json()
     if resp.status_code == 200:
         if data["totalResults"] == 0:
             return render_template('index.html', notFound=True)
         else:
+            data = resp.json()
             print(data)
-            return str(data["items"][0]["value"]["currentRepresentation"]["member"]["value"]["id"])
+            mp = json.loads(data)
+            print(mp[])
+            return 
     else:
         return 400
-    return render_template('search.html',
-                           postcode=postcode)
 
 if __name__ == '__main__':
     app.run(debug=True)
