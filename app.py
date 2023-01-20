5fbb08e4e0d3c7d92c986d26a37050794bf4dc52
toby nott <toby@tobynott.com>
2023-01-20 14:40:17 +0000

    added api functionality

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
-
 from flask import Flask, render_template, request
+import requests
+import jso
+import jso
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -9,6 +12,17 @@ def hello():
 @app.route('/search')
 def search():
     postcode = request.args.get('search')
+    url = "https://members-api.parliament.uk/api/Location/Constituency/Search?searchText="+ postcode
+    resp = requests.get(url)
+    data = resp.json()
+    if resp.status_code == 200:
+        if data["totalResults"] == 0:
+            return render_template('index.html', notFound=True)
+        else:
+            print(data)
+            return str(data["items"][0]["value"]["currentRepresentation"]["member"]["value"]["id"])
+    else:
+        return 400
     return render_template('search.html',
                            postcode=postcode)
 
