b6beb470da84c77c97ac448356a14484b4fb78cc
Toby Nott <tobynott80@gmail.com>
2023-05-15 19:54:47 +0100

    fixed issue with herku not passing through env variable

--- app/app.py
+++ app/app.py
@@ -71,11 +71,11 @@ def search():
     else:
         return 400
 
-if __name__ == '__main__':
-    if os.environ.get('GUARDIAN_KEY') == None:
-        guardianKey = str(input("Please enter the guardian API key \n Note: leave this blank to disable news functionality: "))
-        app.run(host='0.0.0.0', port=80)
-    else:
-        guardianKey = os.environ.get('GUARDIAN_KEY')
-        print("Set guardian API key as " + guardianKey)
-        app.run(host='0.0.0.0', port=80)
+
+if os.environ.get('GUARDIAN_KEY') == None:
+    guardianKey = str(input("Please enter the guardian API key \n Note: leave this blank to disable news functionality: "))
+    app.run(host='0.0.0.0', port=80)
+else:
+    guardianKey = os.environ.get('GUARDIAN_KEY')
+    print("Set guardian API key as " + guardianKey)
+    app.run(host='0.0.0.0', port=80)
