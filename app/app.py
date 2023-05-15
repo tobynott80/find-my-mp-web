d3ba1a0c558217a8deadf11d250a7fe635ac3b00
Toby Nott <tobynott80@gmail.com>
2023-05-15 19:36:38 +0100

    fixed small issue with environment variables

--- app/app.py
+++ app/app.py
@@ -72,7 +72,7 @@ def search():
         return 400
 
 if __name__ == '__main__':
-    if os.getenv('GUARDIAN_KEY') == None:
+    if os.environ.get('GUARDIAN_KEY') == None:
         guardianKey = str(input("Please enter the guardian API key \n Note: leave this blank to disable news functionality: "))
         app.run(host='0.0.0.0', port=80)
     else:
